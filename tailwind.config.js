ebcf78fad0a27b976a35bfbe136b814a363eee5a
Parshuram Das <parshuram.das@ahytech.com>
2026-02-20 14:49:53 +0530

    Second Commit after changes in the background

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
         'facebook': 'hsl(208, 92%, 53%)',
         'twitter': 'hsl(203, 89%, 53%)',
         'youtube': 'hsl(348, 97%, 39%)',
+        'bg-top': '#f8f9fe',
+        'bg-top-dark': '#1f212e',
       },
       backgroundImage: {
         'instagram': 'linear-gradient(to right, hsl(37, 97%, 70%), hsl(5, 77%, 71%), hsl(329, 70%, 58%))',
